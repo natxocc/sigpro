af5bd1a53724773afbb97598033d3ebaeae5d719
natxocc <natxocc@gmail.com>
2026-04-26 15:38:26 +0200

    Tree shaking ESM autoimport IIFE

--- sigpro.js
+++ sigpro.js
@@ -577,14 +577,17 @@ const mount = (comp, target) => {
   return inst
 }
 
-const SigPro = Object.freeze({ $, $$, watch, h, when, each, fx, router, req, mount, batch })
+const sigproFn = Object.freeze({ $, $$, watch, h, when, each, fx, router, req, mount, batch })
 
-if (typeof window !== "undefined") {
-  Object.assign(window, SigPro)
-  "a abbr article aside audio b blockquote br button canvas caption cite code col colgroup datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 header hr i iframe img input ins kbd label legend li main mark meter nav object ol optgroup option output p picture pre progress section select slot small source span strong sub summary sup svg table tbody td template textarea tfoot th thead time tr u ul video"
-    .split(" ").forEach(tag => {
-      window[tag] = (props, children) => h(tag, props, children)
-    })
+const sigpro = () => {
+  if (typeof window !== "undefined") {
+    Object.assign(window, sigproFn)
+    "a abbr article aside audio b blockquote br button canvas caption cite code col colgroup datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 header hr i iframe img input ins kbd label legend li main mark meter nav object ol optgroup option output p picture pre progress section select slot small source span strong sub summary sup svg table tbody td template textarea tfoot th thead time tr u ul video"
+      .split(" ").forEach(tag => { window[tag] = (props, children) => h(tag, props, children) })
+    console.log("SigPro DX installed.")
+  }
 }
 
-export { $, $$, watch, h, when, each, fx, router, req, mount, batch }
+if (typeof import.meta === 'undefined' && typeof window !== 'undefined') sigpro()
+
+export { sigpro, $, $$, watch, h, when, each, fx, router, req, mount, batch }
