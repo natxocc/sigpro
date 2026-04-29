f98cb19ee1aaf925f745cd062ee3c01d82d27a3e
natxocc <natxocc@gmail.com>
2026-04-29 22:54:00 +0200

    Increase exports

--- dist/sigpro.esm.js
+++ dist/sigpro.esm.js
@@ -514,7 +514,7 @@ var mount = (comp, target) => {
   return inst;
 };
 if (typeof window !== "undefined") {
-  Object.assign(window, { $, $$, watch, h, when, each, router, mount, batch });
+  Object.assign(window, { $, $$, watch, h, when, each, router, mount, batch, onUnmount, isArr, isFunc, isObj });
   "a abbr article aside audio b blockquote br button canvas caption cite code col colgroup datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 header hr i iframe img input ins kbd label legend li main mark meter nav object ol optgroup option output p picture pre progress section select slot small source span strong sub summary sup svg table tbody td template textarea tfoot th thead time tr u ul video".split(" ").forEach((tag) => {
     window[tag] = (props, children) => h(tag, props, children);
   });
@@ -525,6 +525,9 @@ export {
   router,
   onUnmount,
   mount,
+  isObj,
+  isFunc,
+  isArr,
   h,
   each,
   batch,
