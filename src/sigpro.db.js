037f8aba24437a1e041820b41fc3eacabb26bdc4
Natxo <natxocc@gmail.com>
2026-09-12 22:38:38 +0200

    Include GET when no data is provided, for simple gets

--- src/sigpro.db.js
+++ src/sigpro.db.js
@@ -1,17 +1,27 @@
-export const db = async (url, data = {}, loading = null) => {
+export const db = async (url, data = null, loading = null) => {
   if (loading) loading(true);
   try {
-    const res = await fetch(url, {
-      method: 'POST',
+    const options = {
+      method: data ? 'POST' : 'GET',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
       credentials: 'include'
-    });
+    };
+    
+    if (data) options.body = JSON.stringify(data);
+
+    const res = await fetch(url, options);
+    
     if (!res.ok) {
       const errorText = await res.text();
       throw new Error(`Error ${res.status}: ${errorText}`);
     }
-    return await res.json();
+
+    const contentType = res.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+      return await res.json();
+    }
+    return await res.text();
+
   } finally {
     if (loading) loading(false);
   }
